561434d3aaf570f3a7f0771f9e71c80945f02a4d
Alex Carolan <alexcarolan@alexs-mbp.home>
2021-01-26 20:08:44 +0000

    content updates

---
+++ js/years-from.js
@@ -0,0 +1,3 @@
+let text = "Hi there! I'm Alex, a software developer currently working in the finance industry with over " + (new Date().getFullYear() - 2019) + " years experience.";
+document.getElementById("years-worked").innerHTML = text;
+
